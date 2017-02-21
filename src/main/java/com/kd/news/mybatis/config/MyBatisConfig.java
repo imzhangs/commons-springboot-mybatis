6654a0a07d1c9452cf49752bd5ed2b1baf3a1ce7
zhangshuai <shit_qq@163.com>
2017-02-21 15:30:03 +0800

    first commit

---
+++ src/main/java/com/kd/news/mybatis/config/MyBatisConfig.java
@@ -0,0 +1,60 @@
+package com.kd.news.mybatis.config;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
+
+@Configuration
+@EnableTransactionManagement
+public class MyBatisConfig implements TransactionManagementConfigurer {
+
+	@Value("${mybatis.mapper-locations}")
+	String mapperLocations;
+
+	@Value("${mybatis.type-aliases-package}")
+	String typeAliasesPackage;
+
+	@Bean
+	public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+		return new SqlSessionTemplate(sqlSessionFactory);
+	}
+
+	@Bean
+	@Override
+	public PlatformTransactionManager annotationDrivenTransactionManager() {
+		return new DataSourceTransactionManager(dataSource);
+	}
+
+	@Autowired
+	DataSource dataSource;
+
+	@Bean(name = "sqlSessionFactory")
+	public SqlSessionFactory sqlSessionFactoryBean() {
+		SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
+		bean.setDataSource(dataSource);
+		bean.setTypeAliasesPackage(typeAliasesPackage);
+
+		// 添加XML目录
+		ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+		try {
+			bean.setMapperLocations(resolver.getResources(mapperLocations));
+			return bean.getObject();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+
+}
